ffae399741b67dd602063404f53d4145b9fbfd4f
Vipul Kumar Verma <vipulkverma1@gmail.com>
2026-03-30 00:24:09 +0530

    Added comments to improve code readability
    Added comments to methods for better clarity.

--- TaskManager.java
+++ TaskManager.java
@@ -9,12 +9,14 @@ public class TaskManager {
         nextId = 1;
     }
 
+    // method to add new task
     public void addTask(String title) {
         Task task = new Task(nextId++, title);
         tasks.add(task);
         System.out.println("Task added successfully!");
     }
 
+    // method to view task
     public void viewTasks() {
         if (tasks.isEmpty()) {
             System.out.println("No tasks available.");
@@ -26,6 +28,7 @@ public class TaskManager {
         }
     }
 
+    // method to mark task as completed
     public void markTaskCompleted(int id) {
         for (Task task : tasks) {
             if (task.getId() == id) {
@@ -37,6 +40,7 @@ public class TaskManager {
         System.out.println("Task not found.");
     }
 
+    // method to delete task
     public void deleteTask(int id) {
         for (Task task : tasks) {
             if (task.getId() == id) {
